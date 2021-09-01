700d950e62157f2f808eebb602f94701cdc21f2c
Praveen8432 <praveen8432@gmail.com>
2021-09-01 22:06:17 +0530

    updated file

--- entry/src/main/java/java/com/github/pwittchen/infinitescroll/app/slice/MainAbilitySlice.java
+++ entry/src/main/java/java/com/github/pwittchen/infinitescroll/app/slice/MainAbilitySlice.java
@@ -20,6 +20,9 @@ import ohos.aafwk.ability.AbilitySlice;
 import ohos.aafwk.content.Intent;
 import java.com.github.pwittchen.infinitescroll.app.ResourceTable;
 
+/**
+ * MainAbilitySlice class to load main layout.
+ */
 public class MainAbilitySlice extends AbilitySlice {
 
     /**
